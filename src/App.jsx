c966f487baabb2e370acd23184295fd5e6a5be5c
Sulyman Khalil <sulyman.hkalil@multiconnect.com.hk>
2026-01-14 11:21:46 +0500

    Inital commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "./components/home";
+import Header from "./components/header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <nav>
+            <h1>ADD TO DO</h1>
+        </nav>
+    );
+};
+
+export default Header;
